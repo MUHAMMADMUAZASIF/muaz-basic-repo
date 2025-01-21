1ba1c71f0952219e49bfeb49bb60a4a9f45990de
muaz <mmuazasif8@gmail.com>
2025-01-22 02:09:38 +0500

    code of c++

---
+++ maximum/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//input values
+int a;
+cout<<"value of a is:";
+cin>>a;
+
+int b;
+cout<<"value of b is:";
+cin>>b;
+
+//condition
+    if(a>b)
+    {
+        cout<<"maximum is :"<<a<<endl;
+    }
+    else
+    {
+        cout<<"maximum is :"<<b<<endl;
+    }
+    return 0;
+}
